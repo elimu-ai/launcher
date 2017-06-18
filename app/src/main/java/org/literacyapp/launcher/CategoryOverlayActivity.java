b456c4207b8aa4f910bc04d876eb05dc584f3804
Jo Grimstad <jo@literacyapp.org>
2017-06-18 16:40:12 +0200

    #10 Display overlay with apps for the tablet navigation category

---
+++ app/src/main/java/org/literacyapp/launcher/CategoryOverlayActivity.java
@@ -0,0 +1,13 @@
+package org.literacyapp.launcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CategoryOverlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_overlay);
+    }
+}
--- app/src/main/java/org/literacyapp/launcher/HomeScreensActivity.java
+++ app/src/main/java/org/literacyapp/launcher/HomeScreensActivity.java
@@ -2,12 +2,17 @@ package org.literacyapp.launcher;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,27 +20,20 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
 import com.andraskindler.parallaxviewpager.ParallaxViewPager;
 import com.matthewtamlin.sliding_intro_screen_library.indicators.DotIndicator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HomeScreensActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ParallaxViewPager viewPager;
-
-    private ImageView egraOralVocabularyImageView;
 
     private DotIndicator dotIndicator;
 
@@ -55,14 +53,6 @@ public class HomeScreensActivity extends AppCompatActivity {
         viewPager = (ParallaxViewPager) findViewById(R.id.container);
         viewPager.setBackgroundResource(R.drawable.background);
         viewPager.setAdapter(mSectionsPagerAdapter);
-
-        egraOralVocabularyImageView = (ImageView) findViewById(R.id.egraOralVocabularyImageView);
-//        // Animate subtle movements
-//        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(egraOralVocabularyImageView, "rotation", 2);
-//        objectAnimator.setDuration(1000);
-//        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
-//        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
-//        objectAnimator.start();
 
         dotIndicator = (DotIndicator) findViewById(R.id.dotIndicator);
 
@@ -88,23 +78,19 @@ public class HomeScreensActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private RelativeLayout tabletNavigationContainer;
+        private ImageView tabletNavigationImageView;
+
+        private RelativeLayout egraOralVocabularyContainer;
+        private ImageView egraOralVocabularyImageView;
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -124,16 +110,97 @@ public class HomeScreensActivity extends AppCompatActivity {
             int layoutIdentifier = getResources().getIdentifier("fragment_home_screen" + String.valueOf(sectionNumber), "layout", getActivity().getPackageName());
             View rootView = inflater.inflate(layoutIdentifier, container, false);
 
+            if (sectionNumber == 1) {
+                tabletNavigationContainer = (RelativeLayout) rootView.findViewById(R.id.tabletNavigationContainer);
+                tabletNavigationImageView = (ImageView) rootView.findViewById(R.id.tabletNavigationImageView);
+                tabletNavigationImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(getClass().getName(), "tabletNavigationImageView onClick");
 
+//                        Intent intent = new Intent(getContext(), CategoryOverlayActivity.class);
+//                        startActivity(intent);
+
+                        MaterialDialog materialDialog = new MaterialDialog.Builder(getContext())
+                                .customView(R.layout.dialog_apps, true)
+                                .theme(Theme.DARK)
+                                .show();
+
+                        View customView = materialDialog.getCustomView();
+                        GridLayout appGridLayout = (GridLayout) customView.findViewById(R.id.appGridLayout);
+
+                        // Fetch apps for category
+                        // TODO: fetch dynamically from Appstore
+                        List<String> packageNames = Arrays.asList(
+                                "com.android.camera2",
+                                "com.android.gallery3d",
+                                "fr.tvbarthel.apps.cameracolorpicker.foss.kids",
+                                "org.literacyapp.startguide",
+                                "org.literacyapp.tilt"
+                        );
+                        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                        final PackageManager packageManager = getActivity().getPackageManager();
+                        List<ResolveInfo> availableActivities = packageManager.queryIntentActivities(intent, 0);
+                        for (ResolveInfo resolveInfo : availableActivities) {
+                            final String packageName = resolveInfo.activityInfo.packageName;
+                            Log.i(getClass().getName(), "packageName: " + packageName);
+                            CharSequence label = resolveInfo.loadLabel(packageManager);
+                            Log.i(getClass().getName(), "label: " + label);
+                            Drawable icon = resolveInfo.loadIcon(packageManager);
+                            Log.i(getClass().getName(), "icon: " + icon);
+
+                            if (packageNames.contains(packageName)) {
+                                View appView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_apps_app_view, appGridLayout, false);
+
+                                ImageView appIconImageView = (ImageView) appView.findViewById(R.id.appIconImageView);
+                                appIconImageView.setImageDrawable(icon);
+
+                                appIconImageView.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Log.i(getClass().getName(), "appIconImageView onClick");
+
+                                        Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                                appGridLayout.addView(appView);
+                            }
+                        }
+                    }
+                });
+
+                egraOralVocabularyContainer = (RelativeLayout) rootView.findViewById(R.id.egraOralVocabularyContainer);
+                egraOralVocabularyImageView = (ImageView) rootView.findViewById(R.id.egraOralVocabularyImageView);
+                egraOralVocabularyImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(getClass().getName(), "egraOralVocabularyImageView onClick");
+
+
+                    }
+                });
+            }
 
             return rootView;
         }
+
+        @Override
+        public void onStart() {
+            Log.i(getClass().getName(), "onCreateView");
+            super.onStart();
+
+            // Add subtle movements to the space ships
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(egraOralVocabularyContainer, "rotation", 2);
+            objectAnimator.setDuration(1000);
+            objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+            objectAnimator.start();
+        }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -142,14 +209,11 @@ public class HomeScreensActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
-            // Show 4 total pages.
             return 4;
         }
 
